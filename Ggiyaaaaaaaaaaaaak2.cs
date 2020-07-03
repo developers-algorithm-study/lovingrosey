1249d7b7cbebcdeed65f1eb771e6c3bb06885547
YeopddeokDouble <67779919+YeopddeokDouble@users.noreply.github.com>
2020-07-03 17:28:28 +0900

    Add files via upload

---
+++ Ggiyaaaaaaaaaaaaak2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ggiyaaaaaaaaaaaaak2 : MonoBehaviour
+{
+    //Baekjoon 2
+
+    void Start()
+    {
+        Debug.Log("강한친구 대한육군");
+        Debug.Log("강한친구 대한육군");
+    }
+    
+}
