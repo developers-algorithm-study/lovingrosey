5588f09616197caf4f46b40b07d7d135fdf4c597
YeopddeokDouble <67779919+YeopddeokDouble@users.noreply.github.com>
2020-07-03 18:00:04 +0900

    Update Ggiyaaaaaaaaaaaaak8.cs

--- Ggiyaaaaaaaaaaaaak8.cs
+++ Ggiyaaaaaaaaaaaaak8.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class Ggiyaaaaaaaaaaaaak8 : MonoBehaviour
 {
     //Baekjoon 1330
-
-    int A = 1;
-    int B = 2;
+    
+    int A;
+    int B;
     
     void Start()
     {
+        A = Random.Range(-10000, 10001);
+        B = Random.Range(-10000, 10001);
+        
         if (A < B) Debug.Log("<");
-
-        A = 10;
-
-        if (A > B) Debug.Log(">");
-
-        A = 5;
-        B = 5;
-
-        if (A == B) Debug.Log("==");
+        else if (A > B) Debug.Log(">");
+        else if (A == B) Debug.Log("==");
     }
     
 }
