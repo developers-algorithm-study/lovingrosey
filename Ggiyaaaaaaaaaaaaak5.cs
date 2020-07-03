76a44e279dbf5cbab3797e321dcab99a969b682a
YeopddeokDouble <67779919+YeopddeokDouble@users.noreply.github.com>
2020-07-03 17:38:07 +0900

    Add files via upload

---
+++ Ggiyaaaaaaaaaaaaak5.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ggiyaaaaaaaaaaaaak5 : MonoBehaviour
+{
+    //Baekjoon 10998
+
+    int A = 1;
+    int B = 2;
+
+    void Start()
+    {
+        Debug.Log(A*B);
+
+        A = 3;
+        B = 4;
+
+        Debug.Log(A*B);
+    }
+    
+}
