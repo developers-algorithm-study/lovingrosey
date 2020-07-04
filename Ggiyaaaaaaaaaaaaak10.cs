a956efda774995cd940caf746ce2c67eacfa484f
YeopddeokDouble <67779919+YeopddeokDouble@users.noreply.github.com>
2020-07-04 11:09:38 +0900

    Add files via upload

---
+++ Ggiyaaaaaaaaaaaaak10.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ggiyaaaaaaaaaaaaak10 : MonoBehaviour
+{
+    //Baekjoon 2753
+
+    int year;
+    
+    void Start()
+    {
+        year = Random.Range(1, 4001);
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) Debug.Log("1");
+        else Debug.Log("0");
+    }
+    
+}
