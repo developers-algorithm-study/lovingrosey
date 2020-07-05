60de7466abd0158fb10acb8031cb835d57d7488d
YeopddeokDouble <67779919+YeopddeokDouble@users.noreply.github.com>
2020-07-05 12:36:48 +0900

    Add files via upload

---
+++ Ggiyaaaaaaaaaaaaak13.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ggiyaaaaaaaaaaaaak13 : MonoBehaviour
+{
+    //Baekjoon 10950
+
+    int T;
+
+    int A;
+    int B;
+        
+
+    void Start()
+    {
+        T = Random.Range(1, 11);
+        Debug.Log(T);
+
+        for (F = 0; F < T; F++)
+        {
+            A = Random.Range(1, 11);
+            B = Random.Range(1, 11);
+            Debug.Log(A+B);
+        }
+    }
+}
